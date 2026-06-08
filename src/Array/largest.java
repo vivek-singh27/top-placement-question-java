d016df32225748a2b5d1eb8dee6c2aa76cc635c3
Vivek Singh <viveksingh93304@gmail.com>
2026-06-09 00:17:01 +0530

    Implement largest number finder with user input
    Fixes the largest number finding logic and adds user input functionality.

--- src/Array/largest.java
+++ src/Array/largest.java
@@ -1,4 +1,27 @@
 package Array;
 
+import java.util.Scanner;
+
 public class largest {
+    public  static int  largest(int []arr,int n){
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the length of the array");
+        int n=sc.nextInt();
+        int[]arr=new int[n];
+        System.out.println("Enter the elements of the array");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int ans=largest(arr,n);
+        System.out.println(ans);
+    }
 }
