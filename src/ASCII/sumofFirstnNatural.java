5fde7a0db56715c8414f1b0690363e4b2b1e519a
Vivek Singh <viveksingh93304@gmail.com>
2026-06-09 00:18:02 +0530

    Update sumofFirstnNatural.java

--- src/ASCII/sumofFirstnNatural.java
+++ src/ASCII/sumofFirstnNatural.java
@@ -1,4 +1,15 @@
 package ASCII;
 
+import java.util.Scanner;
+
 public class sumofFirstnNatural {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
 }
