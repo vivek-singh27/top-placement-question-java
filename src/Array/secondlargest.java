aa121726ba24d0482e743fe7819a34abcb8729b8
Vivek Singh <viveksingh93304@gmail.com>
2026-06-09 00:17:34 +0530

    Add second largest element finder in secondlargest.java
    Implement a method to find the second largest element in an array.

--- src/Array/secondlargest.java
+++ src/Array/secondlargest.java
@@ -1,4 +1,30 @@
 package Array;
 
+import java.util.Scanner;
+
 public class secondlargest {
+    public static int second(int[]arr,int n){
+        int largest=Integer.MIN_VALUE;
+        int second=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+           if(arr[i]>largest){
+               second=largest;
+               largest=arr[i];
+           }else if(arr[i]>second && arr[i]!=largest){
+               second=arr[i];
+           }
+        }
+        return second;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the length of the array");
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int ans=second(arr,n);
+        System.out.println(ans);
+    }
 }
