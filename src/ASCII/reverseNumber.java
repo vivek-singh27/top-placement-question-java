368dda60967293b5b6ad11da5f3e9ce950943cc9
Vivek Singh <viveksingh93304@gmail.com>
2026-06-10 21:08:32 +0530

    Add: reverseNumber -ASCII

---
+++ src/ASCII/reverseNumber.java
@@ -0,0 +1,21 @@
+package ASCII;
+
+import java.util.Scanner;
+
+public class reverseNumber {
+    public static int reverse(int n) {
+        int rev = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n = n / 10;
+        }
+        return rev;
+    }
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        System.out.println(reverse(n));
+    }
+}
